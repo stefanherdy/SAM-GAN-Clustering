31ecb2f0681b3da5eae70021b2a0b6933fb99361
stefanherdy <stefan.herdy@gmail.com>
2023-07-03 20:33:12 +0200

    comment rm scale

--- sam.py
+++ sam.py
@@ -141,6 +141,6 @@ for subdir in os.listdir(root_folder):
                     # If the mask is at the corner of the images we know that the mask is the background mask and do not save it.
                     # Use coordinates [10,10] instead of [0,0], because the model sometimes has problems with the image edges and labels the first few pixel rows incorrect
                     if remaining_mask[10,10] == True:
-                        remove_scale(image_new)
+                        #remove_scale(image_new)
                         cv2.imwrite(destination_folder + '/' + subdir + '/'  + os.path.splitext(image_name)[0] + '_rem' + str(i) + '.png', image_new)
 
