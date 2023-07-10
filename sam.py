4fb173e8a458c3d853a3d8cffce84e9dc2364e22
stefanherdy <stefan.herdy@gmail.com>
2023-07-10 15:14:08 +0200

    rel memory

--- sam.py
+++ sam.py
@@ -142,4 +142,6 @@ for subdir in os.listdir(root_folder):
                     if remaining_mask[10,10] == True:
                         # image_new = remove_scale(image_new)
                         cv2.imwrite(destination_folder + '/' + subdir + '/'  + os.path.splitext(image_name)[0] + '_rem' + str(i) + '.png', image_new)
-
+            del image
+            if len(ex) == 0:
+                del image_new, image_orig
