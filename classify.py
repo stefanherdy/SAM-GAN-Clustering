bf2c6bfba176cb503f19ed4617aea698add08655
stefanherdy <stefan.herdy@gmail.com>
2023-08-04 12:58:30 +0200

    add pytorch classifier

---
+++ classify.py
@@ -0,0 +1,73 @@
+import os
+import torch
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+from torchvision.models import resnet50
+from torch.utils.data import DataLoader, random_split
+import torch.optim as optim
+import torch.nn as nn
+
+# Set the root path to your data
+root_data_path = './riccia_imgs_cropped/'
+
+# Check if a GPU is available, else use CPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Define transformations
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+# Create the dataset
+dataset = ImageFolder(root=root_data_path, transform=transform)
+
+# Split the dataset into training and validation sets
+ratio = 0.8
+num_total = len(dataset)
+num_train = int(ratio * num_total)
+num_val = num_total - num_train
+train_dataset, val_dataset = random_split(dataset, [num_train, num_val])
+
+# Create dataloaders
+train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=8, shuffle=False)
+
+# Load pretrained ResNet-50 model
+model = resnet50(pretrained=True)
+num_classes = len(dataset.classes)
+model.fc = nn.Linear(model.fc.in_features, num_classes)
+
+# Move the model to the available device (GPU or CPU)
+model.to(device)
+
+# Define loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Training loop
+num_epochs = 10
+for epoch in range(num_epochs):
+    model.train()
+    for inputs, labels in train_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs, labels in val_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    accuracy = correct / total
+    print(f'Epoch [{epoch+1}/{num_epochs}], Accuracy: {accuracy:.4f}')
